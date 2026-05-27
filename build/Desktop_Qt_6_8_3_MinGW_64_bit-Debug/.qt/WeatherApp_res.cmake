9d685d34b148d1499c9bd28f2df521ca2933d94e
shimamuraDS <dshworkmeeru@outlook.com>
2026-05-27 10:06:14 +0800

    chore: remove build artifacts, IDE configs, and docs from repo
    Remove ~2200 build files, .idea/, .superpowers/, CMakeLists.txt.user,
    installer exe, and API PDFs. Update .gitignore to prevent re-commit.

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/WeatherApp_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "WeatherApp")
-set(working_dir "E:/WeatherApp")
-set(src_and_dest_list
-
-)
-set(timestamp_file "E:/WeatherApp/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/WeatherApp_res.txt")
--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/bin/qt_setup_tool_path.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=D:\worktool\QT\6.8.3\mingw_64\bin;%PATH%
-%*
